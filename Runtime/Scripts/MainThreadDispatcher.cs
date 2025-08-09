146175d63adee23c1016c6db581100f343d04b07
Nicolas Teichtweier <nicolas.teichtweier@gmail.com>
2025-08-09 03:29:11 +0200

    added comments and error handling to main thread dispatcher

--- Runtime/Scripts/MainThreadDispatcher.cs
+++ Runtime/Scripts/MainThreadDispatcher.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace Schmiggolas.ULogger
 {
+    /// <summary>
+    /// Dispatches actions to be executed on the main Unity thread.
+    /// Useful for calling Unity APIs from background threads.
+    /// </summary>
     public class MainThreadDispatcher : MonoBehaviour
     {
         public static bool IsInitialized { get; private set; }
@@ -21,19 +25,31 @@ namespace Schmiggolas.ULogger
             DontDestroyOnLoad(gameObject);
         }
 
-        public static void Enqueue(Action action)
-        {
-            _actions.Enqueue(action);
-        }
-
         private void Update()
         {
             var iterations = 0;
             while (_actions.TryDequeue(out var action) && iterations < _maxIterationsPerFrame)
             {
-                action?.Invoke();
+                try
+                {
+                    action?.Invoke();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogException(ex, this);
+                }
                 iterations++;
             }
+        }
+
+        /// <summary>
+        /// Enqueues an action to be executed on the main thread.
+        /// Thread-safe and can be called from any thread.
+        /// </summary>
+        /// <param name="action">The action to execute on the main thread</param>
+        public static void Enqueue(Action action)
+        {
+            _actions.Enqueue(action);
         }
     }
 }
